2fef5c414cae206c1f503e3344d15c0dab31a13a
Kaur Samuel Kannel <Kaursamuel.kannel@tptlive.ee>
2019-02-21 09:24:16 +0200

    people class

---
+++ StarWarsApi/StarWarsApi/People.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace StarWarsApi
+{
+    class People
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Height { get; set; }
+        public string Mass { get; set; } 
+        public string HairColor { get; set; }
+        public string SkinColor { get; set; }
+        public string EyeColor { get; set; }
+        public string BirthYear { get; set; }
+        public string Gender { get; set; }
+        public string Homeworld { get; set; }
+
+        public string[] Films { get; set; }
+        public string[] Vehicles { get; set; }
+        public string[] Starships { get; set; }
+    }
+}
