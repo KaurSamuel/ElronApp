3e14f2cd61d5d419f77c112745f21434a809ef89
Virk0 <33309339+Virk0@users.noreply.github.com>
2019-03-05 08:41:28 +0200

    created a customApadter

---
+++ StarWarsApi/StarWarsApi/CustomAdapter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace StarWarsApi
+{
+    public class CustomAdapter : BaseAdapter<string>
+    {
+        string[] items;
+        Activity context;
+
+        public CustomAdapter(Activity context, string[] items) : base()
+        {
+            this.context = context;
+            this.items = items;
+        }
+
+        public override string this[int position]
+        {
+            get { return items[position]; }
+        }
+
+        public override int Count
+        {
+            get { return items.Length; }
+        }
+
+        public override long GetItemId(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- StarWarsApi/StarWarsApi/MainActivity.cs
+++ StarWarsApi/StarWarsApi/MainActivity.cs
@@ -15,6 +15,14 @@ namespace StarWarsApi
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
             dynamic x = ApiSecvice.Get_Single("people/");
+
+        }
+    }
+    public class listActivity : ListActivity
+    {
+        public void List()
+        {
+            //ListAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, ItemInList); 
         }
     }
 }
