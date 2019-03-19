3a8bf987ebefebafb3b3c5b833276b33e38d99e3
Si1mt <siim.tanavots@tptlive.ee>
2019-03-19 14:11:44 +0200

    commented out DetailsAdapter

--- StarWarsApi/StarWarsApi/DetailsAdapter.cs
+++ StarWarsApi/StarWarsApi/DetailsAdapter.cs
@@ -6,51 +6,56 @@ using Android.Widget;
 
 namespace StarWarsApi
 {
-    class DetailsAdapter : BaseAdapter<Dictionary<string,object>>
-    {
-        Dictionary<string, object> items;
-        Activity context;
+    //class DetailsAdapter : BaseAdapter<Dictionary<string,object>>
+    //{
+    //    Dictionary<string, object> items;
+    //    Activity context;
 
 
-        public DetailsAdapter(Activity context, Dictionary<string,object> items) : base()
-        {
-            this.context = context;
-            this.items = items;
-        }
+    //    public DetailsAdapter(Activity context, Dictionary<string,object> items) : base()
+    //    {
+    //        this.context = context;
+    //        this.items = items;
+    //    }
 
-        //public override string this[int position]
-        //{
-        //    get { return items[position]; }
-        //}// => throw new System.NotImplementedException();
+    //    public override Dictionary<string, object> this[string key]
+    //    {
+    //        get { return items[key]; }
+    //    }
 
-        public override Dictionary<string, object> this[string key, object value]
-        {
-            get { return items.ToDictionary(v => v, v => true)[key, value]; }
-        }
+    //    //public override string this[int position]
+    //    //{
+    //    //    get { return items[position]; }
+    //    //}// => throw new System.NotImplementedException();
 
-        public override int Count
-        {
-            get
-            {
-                return items.Count;
-            }
-        }
+    //    //public override Dictionary<string, object> this[string key, object value]
+    //    //{
+    //    //    get { return items.ToDictionary(v => v, v => true)[key, value]; }
+    //    //}
 
-        public override long GetItemId(int position)
-        {
-            return position;
-        }
+    //    public override int Count
+    //    {
+    //        get
+    //        {
+    //            return items.Count;
+    //        }
+    //    }
 
-        public override View GetView(int position, View convertView, ViewGroup parent)
-        {
-            var view = convertView;
-            if (view == null)
-                view = context.LayoutInflater.Inflate(Resource.Layout.details_row, null);
+    //    public override long GetItemId(int position)
+    //    {
+    //        return position;
+    //    }
 
-            view.FindViewById<TextView>(Resource.Id.textView_Key).Text = items[position].key;
-            view.FindViewById<TextView>(Resource.Id.textView_Value).Text = items[position].WeatherDescription;
+    //    public override View GetView(int position, View convertView, ViewGroup parent)
+    //    {
+    //        var view = convertView;
+    //        if (view == null)
+    //            view = context.LayoutInflater.Inflate(Resource.Layout.details_row, null);
 
-            return view;
-        }
-    }
+    //        view.FindViewById<TextView>(Resource.Id.textView_Key).Text = items[position].key;
+    //        view.FindViewById<TextView>(Resource.Id.textView_Value).Text = items[position].WeatherDescription;
+
+    //        return view;
+    //    }
+    //}
 }
