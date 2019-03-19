440cf55e0e9a4406c23e86a57b49f83d1825e3d0
Si1mT <33309403+Si1mT@users.noreply.github.com>
2019-03-19 22:22:03 +0200

    not fully working ItemActivity

--- StarWarsApi/StarWarsApi/ItemActivity.cs
+++ StarWarsApi/StarWarsApi/ItemActivity.cs
@@ -24,11 +24,11 @@ namespace StarWarsApi
             SetContentView(Resource.Layout.DetailsLayout);
 
             string option = Intent.GetSerializableExtra("OptionName").ToString();
-            string itemString = Intent.GetSerializableExtra("ItemName").ToString();
+            string itemName = Intent.GetSerializableExtra("ItemName").ToString();
             string url=Intent.GetSerializableExtra("ItemUrl").ToString();
 
             TextView nameBig = FindViewById<TextView>(Resource.Id.textView_nameBig);
-            nameBig.Text = itemString;
+            nameBig.Text = itemName;
 
             List<string> itemList = new List<string>();
             SharpTrooperCore core = new SharpTrooperCore();
@@ -48,20 +48,30 @@ namespace StarWarsApi
                             continue;
                         itemKey = char.ToUpper(itemKey[0]) + itemKey.Substring(1).Replace("_", " ");
                         itemStrings.PropertyNames.Add(itemKey);
+                        
+                        //if ((List<string>)item.ValStartsWith("https://swapi.co/api/"))
+                        //{
+                        //    string category = item.Value.ToString().Substring(22).TrimEnd('/','1', '2', '3', '4', '5', '6', '7', '8', '9');
+                        //    Type tinghy=Type.GetType(category);
+                        //    SharpEntity result = core.GetSingleByUrl<SharpEntity>(item.Value.ToString());
 
+                        //    itemStrings.PropertyValues.Add(result.ToString());
+                        //}
                         if(item.Value.GetType()==typeof(List<string>))
                         {
-                            string listToString = "";
+                            List<string> stringList = new List<string>();
 
-                            foreach (var item1 in (List<string>)item.Value)
+                            foreach (var listItem in (List<string>)item.Value)
                             {
-                                listToString = item1.ToString();
-                            }
-                            itemStrings.PropertyValues.Add(listToString);
-                        }
-                        else if (item.Value.ToString().StartsWith("https://swapi.co/api/"))
-                        {
+                                SharpTrooper.Entities.
+                                string category = listItem.Substring(21).TrimEnd('/', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+                                Type tinghy = Type.GetType("SharpTrooper.Entities" + category);
+                                (Type.GetType("SharpTrooper.Entities"+category))result = core.GetSingleByUrl<Film>(listItem);
 
+                                stringList.Add(result.title);
+                            }
+                            string listToString = string.Join(", ", stringList);
+                            itemStrings.PropertyValues.Add(listToString);
                         }
                         else
                         {
